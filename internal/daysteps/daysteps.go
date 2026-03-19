326779e8e07ee98828e1318c41f9c1dcc5d6cdfc
Denis Khamzin <96993113+DenisKhamzin@users.noreply.github.com>
2026-03-19 07:24:40 +0300

    parsePackage function is ready

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,6 +1,8 @@
 package daysteps
 
 import (
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -12,7 +14,23 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	duration := time.Duration(0)
+	parseList := strings.Split(data, ",")
+	steps, err := strconv.Atoi(parseList[0])
+	if err != nil {
+		return 0, duration, err
+	}
+	walkTime := strings.Split(strings.Split(parseList[1], "m")[0], "h")
+	hours, err := strconv.Atoi(walkTime[0])
+	if err != nil {
+		return 0, duration, err
+	}
+	minutes, err := strconv.Atoi(walkTime[1])
+	if err != nil {
+		return 0, duration, err
+	}
+	duration = time.Duration(time.Duration(hours)*time.Hour + time.Duration(minutes)*time.Minute)
+	return steps, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
